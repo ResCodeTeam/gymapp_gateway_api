0714ee8a3effef11ef112eb7600e1a300e131f90
roberto barreto <you@example.com>
2022-05-16 16:59:58 +0100

    backend done

--- src/routes/backend.ts
+++ src/routes/backend.ts
@@ -1,64 +1,75 @@
 
 import express from "express";
 import { apiAdapter } from "../apiAdapter";
-const  backendRouter = express.Router();
+const backendRouter = express.Router();
 
 const BASE_URL = 'http://localhost:8000/api/v1';
 const gymapp_api = apiAdapter(BASE_URL);
-            
+
 backendRouter.post('/backend/funcoes', (req, res) => {
   gymapp_api.post(`/backend/funcoes`,
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
+
 backendRouter.post('/backend/cp', (req, res) => {
   gymapp_api.post(`/backend/cp`,
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
+
 backendRouter.post('/backend/registo', (req, res) => {
   gymapp_api.post(`/backend/registo`,
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
+
 backendRouter.post('/backend/atividades', (req, res) => {
   gymapp_api.post(`/backend/atividades`,
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
-backendRouter.delete('/backend/atividades/{atividadeId}', (req, res) => {
-  gymapp_api.delete(`/backend/atividades/{atividadeId}`,
+
+backendRouter.delete('/backend/atividades/:atividadeId', (req, res) => {
+  const atividadeId = req.params.atividadeId;
+  gymapp_api.delete(`/backend/atividades/${atividadeId}`,
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
-backendRouter.put('/backend/atividades/{atividadeId}', (req, res) => {
-  gymapp_api.put(`/backend/atividades/{atividadeId}`,
+
+backendRouter.put('/backend/atividades/', (req, res) => {
+  const atividadeId = req.body.atividadeId;
+  let body = req.body;
+  delete body['atividadeId'];
+  gymapp_api.put(`/backend/atividades/${atividadeId}`,
+    body
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
-backendRouter.put('/backend/musculos/{musculoId}', (req, res) => {
-  gymapp_api.put(`/backend/musculos/{musculoId}`,
+
+backendRouter.put('/backend/musculos/', (req, res) => {
+  const musculoId = req.body.musculoId;
+  let body = req.body;
+  delete body['atividadeId'];
+  gymapp_api.put(`/backend/musculos/${musculoId}`,
+    body
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            
+
 backendRouter.post('/backend/musculos/', (req, res) => {
   gymapp_api.post(`/backend/musculos/`,
+    req.body
   ).then(resp => {
     res.send(resp.data)
   })
 })
-            export { backendRouter }
+
+export { backendRouter }
